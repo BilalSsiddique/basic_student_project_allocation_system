acbfe3d4cfb9f36d331be73962e921e4f8007959
BilalSsiddique <bsiddiqui@students.uit.edu>
2023-08-17 03:39:59 +0500

    updated configuaration

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%n90d4zpxj3dhp6*f4rwq0ss)7@@xs)(rtslo(@^nscpj=+6h#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
